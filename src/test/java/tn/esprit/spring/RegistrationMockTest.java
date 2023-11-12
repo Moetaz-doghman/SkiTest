39309c86244fb7bcafe548b9ffc00af3eb001de6
Moetaz-doghman <61354236+Moetaz-doghman@users.noreply.github.com>
2023-11-12 13:25:20 +0100

    up

--- src/test/java/tn/esprit/spring/RegistrationMockTest.java
+++ src/test/java/tn/esprit/spring/RegistrationMockTest.java
@@ -9,17 +9,18 @@ import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Registration;
 import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.repositories.ICourseRepository;
-import tn.esprit.spring.repositories.IInstructorRepository;
 import tn.esprit.spring.repositories.IRegistrationRepository;
 import tn.esprit.spring.repositories.ISkierRepository;
 import tn.esprit.spring.services.RegistrationServicesImpl;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
-
 
 class RegistrationServicesMockitoTest {
 
@@ -27,14 +28,13 @@ class RegistrationServicesMockitoTest {
     private RegistrationServicesImpl registrationServices;
 
     @Mock
-    private ISkierRepository skierRepository;
-    @Mock
-    private ICourseRepository courseRepository;
+    private IRegistrationRepository registrationRepository;
 
     @Mock
-    private IInstructorRepository instructorRepository;
+    private ISkierRepository skierRepository;
+
     @Mock
-    private IRegistrationRepository registrationRepository;
+    private ICourseRepository courseRepository;
 
     @BeforeEach
     void setUp() {
@@ -42,39 +42,38 @@ class RegistrationServicesMockitoTest {
     }
 
     @Test
-    void addRegistrationAndAssignToSkier() {
-        // Arrange
-        Registration registration = new Registration();
+    void addRegistrationAndAssignToSkierTest() {
         Skier skier = new Skier();
+        Registration registration = new Registration();
         when(skierRepository.findById(anyLong())).thenReturn(Optional.of(skier));
         when(registrationRepository.save(registration)).thenReturn(registration);
 
-        // Act
         Registration result = registrationServices.addRegistrationAndAssignToSkier(registration, 1L);
 
-        // Assert
         verify(skierRepository, times(1)).findById(1L);
         verify(registrationRepository, times(1)).save(registration);
-        assertEquals(result, registration);
+        assertEquals(skier, registration.getSkier());
+        assertEquals(registration, result);
     }
 
     @Test
-    void assignRegistrationToCourse() {
-        // Arrange
+    void assignRegistrationToCourseTest() {
+        Course course = new Course();
         Registration registration = new Registration();
         when(registrationRepository.findById(anyLong())).thenReturn(Optional.of(registration));
-        when(courseRepository.findById(anyLong())).thenReturn(Optional.of(new Course()));
+        when(courseRepository.findById(anyLong())).thenReturn(Optional.of(course));
         when(registrationRepository.save(registration)).thenReturn(registration);
 
-        // Act
         Registration result = registrationServices.assignRegistrationToCourse(1L, 2L);
 
-        // Assert
         verify(registrationRepository, times(1)).findById(1L);
         verify(courseRepository, times(1)).findById(2L);
         verify(registrationRepository, times(1)).save(registration);
-        assertEquals(result, registration);
+        assertEquals(course, registration.getCourse());
+        assertEquals(registration, result);
     }
 
-    // Add more test cases for other methods as needed
+
+
+    // Ajoutez des tests pour d'autres méthodes si nécessaire
 }
